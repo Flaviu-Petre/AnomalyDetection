04cdc764b666fdcbcd65c9d61814f32bc1975b21
Flaviu-Petre <flaviu.petre09@gmail.com>
2026-02-14 19:38:04 +0200

    [Refactor] Migrate app from WinForms to WPF; update service
    Replace WinForms entry points and form with a WPF application skeleton (App.xaml, App.xaml.cs, AssemblyInfo, MainWindow.xaml/.cs) and update the project file to UseWPF (remove UseWindowsForms). Remove old Form1 and Program files. Refactor AnomalyDetectionService: clean up initialization, use ImageSharp image loading/resizing/center-crop, normalize pixels into a DenseTensor, run ONNX inference and compute the max anomaly score, and implement IDisposable. Add project folders (Assets, Models) to the .csproj.

--- anomaly_detection_app/anomaly_detection_app/Models/AnomalyDetectionService.cs
+++ anomaly_detection_app/anomaly_detection_app/Models/AnomalyDetectionService.cs
@@ -7,64 +7,69 @@ using System;
 using System.Collections.Generic;
 using System.Linq;
 
-namespace anomaly_detection_app.Models
+public class AnomalyDetectionService : IDisposable
 {
-    public class AnomalyDetectionService : IDisposable
+    private readonly InferenceSession _session;
+
+    public AnomalyDetectionService(string modelPath)
     {
-        private readonly InferenceSession _session;
+        // Initialize the ONNX session
+        _session = new InferenceSession(modelPath);
+    }
 
-        public AnomalyDetectionService(string modelPath)
-        {
-            _session = new InferenceSession(modelPath);
-        }
+    public float PredictAnomalyScore(string imagePath)
+    {
+        // 1. Load Image
+        using var image = Image.Load<Rgb24>(imagePath);
 
-        public float PredictAnomalyScore(string imagePath)
-        {
-            using var image = SixLabors.ImageSharp.Image.Load<Rgb24>(imagePath);
-
-            image.Mutate(x => x
+        // 2. Preprocess: Resize to 256x256, Center Crop to 224x224
+        image.Mutate(x => x
             .Resize(new ResizeOptions
             {
-                Size = new SixLabors.ImageSharp.Size(224, 224),
+                Size = new Size(256, 256),
                 Mode = ResizeMode.Crop
             })
-            .Crop(new SixLabors.ImageSharp.Rectangle(16, 16, 224, 224)));
+            .Crop(new Rectangle(16, 16, 224, 224)));
 
-            var inputTensor = new DenseTensor<float>(new[] { 1, 3, 224, 224 });
-            var mean = new[] { 0.485f, 0.456f, 0.406f };
-            var std = new[] { 0.229f, 0.224f, 0.225f };
+        // 3. Convert to Tensor and Normalize
+        var inputTensor = new DenseTensor<float>(new[] { 1, 3, 224, 224 });
+        var mean = new[] { 0.485f, 0.456f, 0.406f };
+        var std = new[] { 0.229f, 0.224f, 0.225f };
 
-            for (int y = 0; y < image.Height; y++)
-            {
-                for (int x = 0; x < image.Width; x++)
-                {
-                    var pixel = image[x, y];
-                    inputTensor[0, 0, y, x] = ((pixel.R / 255f) - mean[0]) / std[0];
-                    inputTensor[0, 1, y, x] = ((pixel.G / 255f) - mean[1]) / std[1];
-                    inputTensor[0, 2, y, x] = ((pixel.B / 255f) - mean[2]) / std[2];
-                }
-            }
-
-            var inputs = new List<NamedOnnxValue>
-            {
-                NamedOnnxValue.CreateFromTensor("input", inputTensor)
-            };
-
-            using var results = _session.Run(inputs);
-            var outputTensor = results.First().AsTensor<float>();
-
-            float maxScore = float.MinValue;
-            foreach( var value in outputTensor)
-            {
-                if (value > maxScore) maxScore = value;
-            }
-
-            return maxScore;
-        }
-
-        public void Dispose()
+        for (int y = 0; y < image.Height; y++)
         {
-            _session?.Dispose();
+            for (int x = 0; x < image.Width; x++)
+            {
+                var pixel = image[x, y];
+                // Convert to 0-1 range and apply normalization
+                inputTensor[0, 0, y, x] = ((pixel.R / 255f) - mean[0]) / std[0];
+                inputTensor[0, 1, y, x] = ((pixel.G / 255f) - mean[1]) / std[1];
+                inputTensor[0, 2, y, x] = ((pixel.B / 255f) - mean[2]) / std[2];
+            }
         }
+
+        // 4. Run Inference
+        // The python script defines the input name as "input"
+        var inputs = new List<NamedOnnxValue>
+        {
+            NamedOnnxValue.CreateFromTensor("input", inputTensor)
+        };
+
+        using var results = _session.Run(inputs);
+        var outputTensor = results.First().AsTensor<float>();
+
+        // 5. Calculate Score: Max value of the anomaly map
+        float maxScore = float.MinValue;
+        foreach (var value in outputTensor)
+        {
+            if (value > maxScore) maxScore = value;
+        }
+
+        return maxScore;
+    }
+
+    public void Dispose()
+    {
+        _session?.Dispose();
     }
 }
