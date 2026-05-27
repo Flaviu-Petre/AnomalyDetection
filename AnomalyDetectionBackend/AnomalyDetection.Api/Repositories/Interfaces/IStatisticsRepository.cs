a8354bf717bcbdc3b8bd406016b68abdbc1db675
Flaviu-Petre <flaviu.petre09@gmail.com>
2026-05-27 18:12:02 +0300

    Add IStatisticsRepository and IUserRepository interfaces
    Introduce repository interfaces for statistics and user management.

---
+++ AnomalyDetectionBackend/AnomalyDetection.Api/Repositories/Interfaces/IStatisticsRepository.cs
@@ -0,0 +1,18 @@
+﻿using AnomalyDetection.Api.Models.DTOs;
+using AnomalyDetection.Api.Models.Entities;
+
+namespace AnomalyDetection.Api.Repositories.Interfaces
+{
+    public interface IStatisticsRepository
+    {
+        void AddInferenceRecord(InferenceRecord record);
+        List<InferenceRecord> GetRecordsSince(DateTime startDate);
+        PagedResult<InferenceHistoryDto> GetPagedHistory(
+            DateTime startDate,
+            int? userId,
+            int pageNumber,
+            int pageSize,
+            string sortBy,
+            bool sortDescending);
+    }
+}
---
+++ AnomalyDetectionBackend/AnomalyDetection.Api/Repositories/Interfaces/IUserRepository.cs
@@ -0,0 +1,16 @@
+﻿using AnomalyDetection.Api.Models.Entities;
+
+namespace AnomalyDetection.Api.Repositories.Interfaces
+{
+    public interface IUserRepository
+    {
+        User? GetUserByUsername(string username);
+        User? GetUserById(int userId);
+        List<User> GetAllUsers();
+        void AddUser(User user);
+        void UpdateUserRole(int userId, string newRole);
+        void UpdateUser(User user);
+        void DeleteUser(int userId);
+        bool UserExists(string username);
+    }
+}
